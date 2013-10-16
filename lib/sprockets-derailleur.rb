7590681e19b929ffbc1046947e8ca45ad9f37803
Robert R. Meyer <Blue.Dog.Archolite@gmail.com>
2013-10-16 12:45:07 -0500

    set default log level to error

--- lib/sprockets-derailleur.rb
+++ lib/sprockets-derailleur.rb
@@ -9,7 +9,7 @@ module SpeedUp
 
   def self.get_logger
     logger = Logging.logger(STDOUT)
-    logger_level = ENV["LOGGER_LEVEL"] ? ENV["LOGGER_LEVEL"].to_sym : :warn
+    logger_level = ENV["LOGGER_LEVEL"] ? ENV["LOGGER_LEVEL"].to_sym : :error
     logger.level = logger_level
     logger
   end
